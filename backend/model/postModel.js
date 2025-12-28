ea8f976c5e2c8a90136a622086550d51f4db6987
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-12-28 22:18:39 +0500

    create 4 models (Backend)

---
+++ backend/model/postModel.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const replySchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  message: {
+    type: String,
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date },
+});
+const commentSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    message: {
+      type: String,
+    },
+    replies: {
+      replySchema,
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date },
+  },
+  { _id: true }
+);
+const postSchema = new mongoose.Schema(
+  {
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Channel",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    comments: {
+      commentSchema,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
